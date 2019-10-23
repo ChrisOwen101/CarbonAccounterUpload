5f8738711da0f69991a9e6a4b5a652df03b6f620
Chris Owen <chris.owen.casual@theguardian.com>
2019-10-23 11:08:10 +0100

    Updates

---
+++ precache-manifest.dcf5c876e3af7744700b7419318df390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a3aa8b4c4b086258dc670784c54db7",
+    "url": "/carbonaccounterupload/index.html"
+  },
+  {
+    "revision": "9780362f12c12d574423",
+    "url": "/carbonaccounterupload/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "41153fd1610edc59ae9e",
+    "url": "/carbonaccounterupload/static/js/2.a41f5d54.chunk.js"
+  },
+  {
+    "revision": "9780362f12c12d574423",
+    "url": "/carbonaccounterupload/static/js/main.7117fde8.chunk.js"
+  },
+  {
+    "revision": "6471fb47e7f5a1e63b5e",
+    "url": "/carbonaccounterupload/static/js/runtime-main.863f1674.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/carbonaccounterupload/static/media/logo.25bf045c.svg"
+  }
+]);
